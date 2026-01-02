24a71655ef7b41dbbd1088dc62d1e821de04628d
Adem <devloom45@gmail.com>
2026-01-02 22:48:29 +0100

    bla

--- Nexo-Connecto/resources/js/Application/LandingPage/logos/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as OpenAILogo } from './OpenAILogo';
-export { default as GoogleLogo } from './GoogleLogo';
-export { default as AmazonLogo } from './AmazonLogo';
-export { default as AppleLogo } from './AppleLogo';
-export { default as MetaLogo } from './MetaLogo';
-
